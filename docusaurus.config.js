2f6520cad49dc562ee19edecba9b2b4bd5c8e920
Sukrit Boonin <107474533+tintinchan@users.noreply.github.com>
2022-12-02 00:17:43 +0700

    remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -97,19 +96,6 @@ const config = {
               {
                 label: 'Twitter',
                 href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
               },
             ],
           },
