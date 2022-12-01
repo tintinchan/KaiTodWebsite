896b7cff6ccf0cf8648e5a440e44526c729b7d37
Sukrit Boonin <107474533+tintinchan@users.noreply.github.com>
2022-12-02 00:14:50 +0700

    netlify setup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://kaitod.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -18,8 +18,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'KaiTodWebsite', // Usually your repo name.
-  deploymentBranch: 'deployment',
-  trailingSlash: false,
   
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
